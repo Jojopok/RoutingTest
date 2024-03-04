ef611fbdfa2b5311d3e784dd12692f82610df037
Jojopok <zaiajonathan@gmail.com>
2024-03-04 14:30:12 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'signup', component: SignUpComponent },
+    { path: 'user', component: UserComponent },
+    { path: 'user-profile', component: UserProfileComponent},
+    { path: '', redirectTo: '/user', pathMatch: 'full' },
+
+];
